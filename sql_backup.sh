10fddf38ab875c3924d691d7e655248061f86d72
Riley Nevins <64431703+rileyjnevins@users.noreply.github.com>
2021-10-31 15:38:22 -0500

    Create sql_backup.sh

---
+++ sql_backup.sh
@@ -0,0 +1,19 @@
+## To use this, first (and only once) do chmod +x SQL_Backup.sh
+## Then execute ./SQL_Backup.sh
+##    Enter data as needed, and boom! Done!
+
+## When moving this to a cronjob/automated task, remember to remove any user-input (not very ideal)
+## Instead I'd recommend making a variable = db's you want, or make several crons maybe?
+
+echo "Enter the database you'd like to backup:"
+read selected_db
+
+## Time is so specific it will avoid file overwriting issues.
+today=$(date +"%Y-%m-%d-%I-%S")
+
+echo "You are about to backup the \"$selected_db\" Mongo DB."
+
+## Change this directory to match your desited backup location.
+## Leave selected_db alone here, it's entered by user.
+mongodump --archive=/root/backup/$selected_db-${today}.gz --gzip --db=$selected_db
+echo "Your backup has been saved!"
