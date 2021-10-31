8f6c77623c0b85903ae61d48b0caf5acd7b6ae7b
Riley Nevins <rileyjnevins@gmail.com>
2021-10-31 15:52:15 -0500

    Add checks for DB software

--- sql_backup.sh
+++ sql_backup.sh
@@ -5,15 +5,28 @@
 ## When moving this to a cronjob/automated task, remember to remove any user-input (not very ideal)
 ## Instead I'd recommend making a variable = db's you want, or make several crons maybe?
 
-echo "Enter the database you'd like to backup:"
-read selected_db
+##
+## Detect what SQL service is being used.
+##
 
-## Time is so specific it will avoid file overwriting issues.
-today=$(date +"%Y-%m-%d-%I-%S")
+IS_MONGO=`$(systemctl is-active --quiet mongod)` ## MONGODB
+IS_SQL=`$(systemctl is-active --quiet mysql)` ## MYSQL
+IS_MARIA=`$(systemctl is-active --quiet mongod)` ## MARIADB
 
-echo "You are about to backup the \"$selected_db\" Mongo DB."
+if [$IS_MONGO == "active"]
+then
+  clear
+  echo "Enter the database you'd like to backup:"
+  read selected_db
 
-## Change this directory to match your desited backup location.
-## Leave selected_db alone here, it's entered by user.
-mongodump --archive=/root/backup/$selected_db-${today}.gz --gzip --db=$selected_db
-echo "Your backup has been saved!"
+  ## Time is so specific it will avoid file overwriting issues.
+  today=$(date +"%Y-%m-%d-%I-%S")
+  
+  echo "You are about to backup the \"$selected_db\" Mongo DB."
+
+  ## Change this directory to match your desited backup location.
+  ## Leave selected_db alone here, it's entered by user.
+  mongodump --archive=/root/backup/$selected_db-${today}.gz --gzip --db=$selected_db
+  echo "Your backup has been saved!"
+
+fi
